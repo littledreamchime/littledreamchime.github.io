1d6c9dd0f3a02cf7a88f55062ecf2ecb2bf0c83f
BobSmith <littledreamchime@gmail.com>
2026-05-26 18:18:35 +0800

    refactor(deskitems): add time variable and data target

--- src/store.js
+++ src/store.js
@@ -3,3 +3,8 @@ import { atom } from "nanostores";
 export const isWelcomeDismissed=atom(false);
 
 export const currentView = atom('Home');
+
+export const ANIMATION_TIMES={
+    ZOOM_IN:1200,
+    ZOOM_OUT:3000
+};
